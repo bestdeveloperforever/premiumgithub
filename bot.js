d2813c60b7bfa96805171eda51e1242d708bb9f2
bestdeveloperforever <42047034+bestdeveloperforever@users.noreply.github.com>
2018-11-15 13:49:56 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -10,7 +10,22 @@ client.channels.get("510190261020786733").send(`${ReBeL[Math.floor(Math.random()
 });
 
 
+const prefix = '1';
 
+client.on('message', message => {
+  if (message.author.bot) return;
+  if (!message.content.startsWith(prefix)) return;
+
+  let command = message.content.split(" ")[0];
+  command = command.slice(prefix.length);
+
+  let args = message.content.split(" ").slice(1);
+
+  if (command == "say") {
+   message.channel.sendMessage(args.join("  "))
+   message.delete()
+  }
+ });
 
 
 client.on('ready', () => {
