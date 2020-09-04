96d78ecfd7eea253745f321816ffa864c62bba7a
bestdeveloperforever <42047034+bestdeveloperforever@users.noreply.github.com>
2020-09-04 23:31:52 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,20 +1,65 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
-  client.on('ready', () => {
-  console.log('╚[════════════════════════════════════]╝')
+const bot = new Discord.Client();
+const stripIndents = require('common-tags').stripIndents;
+const fs = require('fs');
+const developers = ["462647886035812372"]
+const adminprefix = "-";
+var moment = require('moment')
+const prefix = "-";
+client.on('ready', () => {
+  console.log('╔[════════════════════════════════════]╗');
+  console.log('')
+  console.log('            ╔[════════════]╗')
+  console.log('              Bot Is Online')
+  console.log('            ╚[════════════]╝')
+  console.log('')
   console.log(`Logged in as ${client.user.tag}!`);
+  console.log('')
+  console.log(`servers! [ " ${client.guilds.size} " ]`);
+  console.log('')
+  console.log('By Osama_DK')
+  console.log('')  
+  console.log(`Users! [ " ${client.users.size} " ]`);
+  console.log('')
   console.log('╚[════════════════════════════════════]╝')
 });
 
 
-client.on("ready", () => {
-  const channel = client.channels.get("751355218209407018");
-  if (!channel) return console.error("The channel does not exist!");
-  channel.join().then(connection => {
-    // Yay, it worked!
-    console.log("Successfully connected.");
-  });
+
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'setplaying')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**Done You Have Been Changed The Playing To ${argresult}**✅ `)
+  } else 
+     if (message.content === (adminprefix + "speedgenbotleave")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'setwatching')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**Done You Have Been Changed The Watching To ${argresult}**✅`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'setlistening')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**Done You Have Been Changed The Listening To ${argresult}**✅`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'setstreaming')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**Done You Have Been Changed The Streaming To ${argresult}**✅`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`**Changing The Name To ..${argresult}** ✅`)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`**Changing The Avatar To :${argresult}** ✅`);
+}
+
+
 });
 
 client.login(process.env.BOT_TOKEN);
