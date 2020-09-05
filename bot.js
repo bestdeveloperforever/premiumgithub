27c2372e59aadb328e7af9ada29009e189a54a55
bestdeveloperforever <42047034+bestdeveloperforever@users.noreply.github.com>
2020-09-05 20:51:12 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,24 +1,44 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const developers = ["742361727387041813"]
+const adminprefix = "-";
 const prefix = "-";
 client.on('ready', () => {
   console.log('╔[════════════════════════════════════]╗');
   console.log('')
-  console.log('            ╔[════════════]╗')
-  console.log('              Bot Is Online')
-  console.log('            ╚[════════════]╝')
-  console.log('')
   console.log(`Logged in as ${client.user.tag}!`);
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log('')
-  console.log('By Osama_DK')
-  console.log('')  
-  console.log(`Users! [ " ${client.users.size} " ]`);
   console.log('')
   console.log('╚[════════════════════════════════════]╝')
 });
 
+
+
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!developers.includes(message.author.id)) return;
+      
+  if (message.content.startsWith(adminprefix + 'setplaying')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**Done You Have Been Changed The Playing To ${argresult}**✅ `)
+  } else 
+     if (message.content === (adminprefix + "leaveserver")) {
+    message.guild.leave();        
+  } else  
+  if (message.content.startsWith(adminprefix + 'setwatching')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**Done You Have Been Changed The Watching To ${argresult}**✅`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'setlistening')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**Done You Have Been Changed The Listening To ${argresult}**✅`)
+  } else 
+  if (message.content.startsWith(adminprefix + 'setstreaming')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**Done You Have Been Changed The Streaming To ${argresult}**✅`)
+  }
+
+
+});
 
 
 client.login(process.env.BOT_TOKEN);
