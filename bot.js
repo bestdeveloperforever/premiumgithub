b5fae304f1ac4063e6bb760a8b323a7363925966
bestdeveloperforever <42047034+bestdeveloperforever@users.noreply.github.com>
2020-09-06 01:37:41 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,7 +33,7 @@ client.on('voiceStateUpdate', (oldMember, newMember) => {
         READ_MESSAGES: true,
         SEND_MESSAGES: true
       }).catch(console.error);
-           client.channels.get("751917878089089044").send("<@" + member.user.oldID + ">");
+           client.channels.get("751917878089089044").send("<@" + oldID + ">");
     } else if (oldID === vcID && newID !== vcID) {   // Left the voice channel.
       textChannel.overwritePermissions(newMember, {
         READ_MESSAGES: false,
